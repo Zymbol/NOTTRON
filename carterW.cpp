c8de5439c5b01e655b07e36a057199460a79ff4f
ceedubyuh <clwomack2000@gmail.com>
2020-10-15 12:20:31 -0700

    added Lab 'work'

--- carterW.cpp
+++ carterW.cpp
@@ -4,6 +4,7 @@
 */
 
 // 10-7-2020 Removed extra include statements
+// 10-15-2020 Tried adding an explosion when the cycle hits a trail/wall  
 
 #include "fonts.h"
 
@@ -14,3 +15,13 @@ void carter(int x, int y) {
 	r.center = 0;
 	ggprint16(&r, 16, 0x00ffffff, "Carter Womack");
 }
+
+/*
+void explodeAndEnd() {
+    if (g.gameover) {
+    render explosion sprites in order
+    spent all class trying to learn how to render and animate sprites using multiple images.
+    yea, I made no progress :)   
+    }
+}
+*/
